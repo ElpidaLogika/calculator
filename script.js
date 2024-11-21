cda5f68b9b8f2f883a127d05ec29bce2b97071ee
elpidalogika <elpidalogika@gmail.com>
2024-11-21 23:50:55 +0200

    change flag

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 let inputField = document.querySelector('.input');
+let isEqual = false;
 
-document.querySelector('.btn-AC').addEventListener('click', () => { inputField.innerHTML = '0' });
+document.querySelector('.btn-AC').addEventListener('click', () => { inputField.innerHTML = '0'});
 document.querySelector('.btn-dot').addEventListener('click', () => { inputField.innerHTML += '.' });
-document.querySelector('.btn-mult').addEventListener('click', () => { inputField.innerHTML += '*' });
-document.querySelector('.btn-div').addEventListener('click', () => { inputField.innerHTML += '/' });
-document.querySelector('.btn-add').addEventListener('click', () => { inputField.innerHTML += '+' });
-document.querySelector('.btn-sub').addEventListener('click', () => { inputField.innerHTML += '-' });
+document.querySelector('.btn-mult').addEventListener('click', () => { inputField.innerHTML += '*'; isEqual = false; });
+document.querySelector('.btn-div').addEventListener('click', () => { inputField.innerHTML += '/'; isEqual = false;});
+document.querySelector('.btn-add').addEventListener('click', () => { inputField.innerHTML += '+'; isEqual = false;});
+document.querySelector('.btn-sub').addEventListener('click', () => { inputField.innerHTML += '-'; isEqual = false; });
 
 document.querySelector('.btn-del').addEventListener('click', () => { inputField.innerHTML = inputField.innerHTML.slice(0, -1) });
 
@@ -20,7 +21,8 @@ document.querySelector('.btn-plus-minus').addEventListener('click', () => {
 
 for (let i = 0; i <= 9; i++) {    
     document.querySelector(`.btn-${i}`).addEventListener('click', () => {
-        if (inputField.innerHTML === '0') {
+        if (inputField.innerHTML === '0' || isEqual) {
+            isEqual = false;
             inputField.innerHTML = `${i}`;
         }
         else { 
@@ -29,5 +31,9 @@ for (let i = 0; i <= 9; i++) {
     });  
 }
 
-document.querySelector('.btn-equal').addEventListener('click', () => { inputField.innerHTML = eval(inputField.innerHTML) });
+document.querySelector('.btn-equal').addEventListener('click', () => {
+    isEqual = true;
+    inputField.innerHTML = eval(inputField.innerHTML)
+});
 
+g
